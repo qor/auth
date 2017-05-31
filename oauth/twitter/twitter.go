6c802d41c610b51f79cdeb519720152367864469
Jinzhu <wosmvp@gmail.com>
2017-05-31 16:18:38 +0800

    Add oauth twitter provider

---
+++ oauth/twitter/twitter.go
@@ -0,0 +1,23 @@
+package twitter
+
+import (
+	"net/http"
+
+	"github.com/qor/auth"
+)
+
+// twitterProvider provide login with twitter method
+type TwitterProvider struct {
+}
+
+// Login implemented login with twitter provider
+func (TwitterProvider) Login(request *http.Request, writer http.ResponseWriter, claims *auth.Claims) {
+}
+
+// Logout implemented logout with twitter provider
+func (TwitterProvider) Logout(request *http.Request, writer http.ResponseWriter, claims *auth.Claims) {
+}
+
+// Register implemented register with twitter provider
+func (TwitterProvider) Register(request *http.Request, writer http.ResponseWriter, claims *auth.Claims) {
+}
