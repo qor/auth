568ca935a5eaf26d7653a8f6c5464f56d94fe887
Jinzhu <wosmvp@gmail.com>
2017-08-04 17:47:53 +0800

    Add Register, Allow method for Authority

--- authority/authority_rule.go
+++ authority/authority_rule.go
@@ -14,14 +14,10 @@ type Rule struct {
 	LoggedAs               []string
 }
 
-// ToHandler generate roles checker
-func (authority Authority) ToHandler(rule Rule) roles.Checker {
+// Handler generate roles checker
+func (authority Authority) Handler(rule Rule) roles.Checker {
 	return func(req *http.Request, user interface{}) bool {
-		claims, err := authority.Auth.GetClaims(req)
-
-		if err != nil {
-			return false
-		}
+		claims, _ := authority.Auth.GetClaims(req)
 
 		// Check Last Auth
 		if rule.TimeoutSinceLastAuth > 0 {
@@ -52,4 +48,15 @@ func (authority Authority) ToHandler(rule Rule) roles.Checker {
 
 		return true
 	}
+}
+
+// Register register authority rule into Role
+func (authority *Authority) Register(name string, rule Rule) {
+	authority.Config.Role.Register(name, authority.Handler(rule))
+}
+
+// Allow Check allow role or not
+func (authority *Authority) Allow(role string, req *http.Request) bool {
+	currentUser := authority.Auth.GetCurrentUser(req)
+	return authority.Role.HasRole(req, currentUser, role)
 }
